21c3b3d7a7ae47db27dfd0fa2d030699f2edea7c
Rea Tabofunda <reacarolyn@gmail.com>
2022-03-14 23:17:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello there, welcome to my Band Name Generator program")
+#user's city name
+city_name = input("What is the name of the City you grew up?\n")
+#user's pet name
+pet_name = input("What is the name of your pet?\n")
+#generate the band name 
+print("Your band name could be " + city_name + " " + pet_name)
+
